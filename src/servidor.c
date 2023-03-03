a5fcf42db908157c0798057d59ca2ffab8ae832f
ldcas-uc3m <100429021@alumnos.uc3m.es>
2023-03-03 10:47:34 +0100

    servidor WIP

--- src/servidor.c
+++ src/servidor.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <mqueue.h>
+#include <pthread.h>
 
 #include "lib/server_impl.h"
 #include "lib/comm.h"
@@ -11,13 +12,52 @@ void tratar_peticion(struct Peticion* p) {
     struct Peticion pet;
     pet = *p;
 
+    // awake main
+
+    // treat petition
     struct Respuesta res;
 
-    // despertar main 
-    
+    switch (pet.opcode) {
+        case 0:
+            res.result = init();
+            break;
+
+        case 1:
+            res.result = set_value(pet.value.clave, pet.value.value1, pet.value.value2, pet.value.value3);
+            break;
+
+        case 2:
+            res.result = get_value(pet.value.clave, &(res.value.value1), &(res.value.value2), &(res.value.value3));
+            break;
+        
+        case 3:
+            res.result = modify_value(pet.value.clave, pet.value.value1, pet.value.value2, pet.value.value3);
+            break;
+        
+        case 4:
+            res.result = exist(pet.value.clave);
+            break;
+
+        case 5:
+            res.result = copy_key(pet.value.clave, pet.alt_key);
+            break;
+        
+        default:
+            res.result = -1;
+            break;
+    }
+
+    // answer
+    mqd_t qc = mq_open(pet.cola_client, O_WRONLY);  // client queue
+    mq_send(qc, &res, sizeof(res), 0);
+    mq_close(qc);
+
+    pthread_exit(NULL);
 }
 
 
 int main() {
+    
+
     return 0;
 }
